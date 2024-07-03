854fb601f565b2f2549e2d7236988dfd59513a1f
Jabborov-Sardor <jabborovsardor0097.net@gmail.com>
2024-07-03 19:10:55 +0500

    yakuniy

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const elInpuit = document.querySelector("#inp");
+const elInput = document.querySelector("#inp");
 const elBtn0 = document.querySelector("#btn0");
 const elBtn1 = document.querySelector("#btn1");
 const elBtn2 = document.querySelector("#btn2");
@@ -11,122 +11,152 @@ const elBtn8 = document.querySelector("#btn8");
 const elBtn9 = document.querySelector("#btn9");
 
 elBtn0.addEventListener("click", function () {
-    if (+elInpuit.value < 10) {
-        elInpuit.value = elInpuit.value + elBtn0.value;
-        document.getElementById('all_rounds').style.border = 'solid red' 
-      }
-      
-      if (+elInpuit.value > 0 && +elInpuit.value < 10 ) {
-        document.getElementById('rounds2').style.border = 'solid red'
-        document.getElementById('all_rounds').style.border = 'none'  
-    }
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn0.value;
+    document.getElementById("all_rounds").style.border = "solid red";
+  }
+  console.log(elInput.value.length, "");
+
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
 });
 
 elBtn1.addEventListener("click", function () {
-      if (+elInpuit.value < 10) {
-        elInpuit.value = elInpuit.value + elBtn1.value;
-        document.getElementById('all_rounds').style.border = 'solid red' 
-      }
-      
-      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-        document.getElementById('rounds2').style.border = 'solid red'
-        document.getElementById('all_rounds').style.border = 'none'  
-    }
-    });
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn1.value;
+    document.getElementById("all_rounds").style.border = "solid red";
+  }
 
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
+  //
+  console.log(elInput.value);
+
+  if (elInput.value.includes("0")) {
+    elInput.value = Number(elInput.value);
+  }
+});
 
 elBtn2.addEventListener("click", function () {
-  if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + elBtn2.value;
-    document.getElementById('all_rounds').style.border = 'solid red' 
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn2.value;
+    document.getElementById("all_rounds").style.border = "solid red";
   }
-  
-  if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-    document.getElementById('rounds2').style.border = 'solid red'
-    document.getElementById('all_rounds').style.border = 'none'  
-}
+
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
+  if (elInput.value.includes("0")) {
+    elInput.value = Number(elInput.value);
+  }
 });
 
 elBtn3.addEventListener("click", function () {
-  if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + elBtn3.value;
-    document.getElementById('all_rounds').style.border = 'solid red' 
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn3.value;
+    document.getElementById("all_rounds").style.border = "solid red";
   }
-  
-  if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-    document.getElementById('rounds2').style.border = 'solid red'
-    document.getElementById('all_rounds').style.border = 'none'  
-}
+
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
+  if (elInput.value.includes("0")) {
+    elInput.value = Number(elInput.value);
+  }
 });
 
 elBtn4.addEventListener("click", function () {
-    if (+elInpuit.value < 10) {
-        elInpuit.value = elInpuit.value + elBtn4.value;
-        document.getElementById('all_rounds').style.border = 'solid red' 
-      }
-      
-      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-        document.getElementById('rounds2').style.border = 'solid red'
-        document.getElementById('all_rounds').style.border = 'none'  
-    }
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn4.value;
+    document.getElementById("all_rounds").style.border = "solid red";
+  }
+
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
+  if (elInput.value.includes("0")) {
+    elInput.value = Number(elInput.value);
+  }
 });
 
 elBtn5.addEventListener("click", function () {
-    if (+elInpuit.value < 10) {
-        elInpuit.value = elInpuit.value + elBtn5.value;
-        document.getElementById('all_rounds').style.border = 'solid red' 
-      }
-      
-      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-        document.getElementById('rounds2').style.border = 'solid red'
-        document.getElementById('all_rounds').style.border = 'none'  
-    }
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn5.value;
+    document.getElementById("all_rounds").style.border = "solid red";
+  }
+
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
+  if (elInput.value.includes("0")) {
+    elInput.value = Number(elInput.value);
+  }
 });
 
 elBtn6.addEventListener("click", function () {
-    if (+elInpuit.value < 10) {
-        elInpuit.value = elInpuit.value + elBtn6.value;
-        document.getElementById('all_rounds').style.border = 'solid red' 
-      }
-      
-      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-        document.getElementById('rounds2').style.border = 'solid red'
-        document.getElementById('all_rounds').style.border = 'none'  
-    }
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn6.value;
+    document.getElementById("all_rounds").style.border = "solid red";
+  }
+
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
+  if (elInput.value.includes("0")) {
+    elInput.value = Number(elInput.value);
+  }
 });
 
 elBtn7.addEventListener("click", function () {
-    if (+elInpuit.value < 10) {
-        elInpuit.value = elInpuit.value + elBtn7.value;
-        document.getElementById('all_rounds').style.border = 'solid red' 
-      }
-      
-      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-        document.getElementById('rounds2').style.border = 'solid red'
-        document.getElementById('all_rounds').style.border = 'none'  
-    }
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn7.value;
+    document.getElementById("all_rounds").style.border = "solid red";
+  }
+
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
+  if (elInput.value.includes("0")) {
+    elInput.value = Number(elInput.value);
+  }
 });
 
 elBtn8.addEventListener("click", function () {
-    if (+elInpuit.value < 10) {
-        elInpuit.value = elInpuit.value + elBtn8.value;
-        document.getElementById('all_rounds').style.border = 'solid red' 
-      }
-      
-      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-        document.getElementById('rounds2').style.border = 'solid red'
-        document.getElementById('all_rounds').style.border = 'none'  
-    }
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn8.value;
+    document.getElementById("all_rounds").style.border = "solid red";
+  }
+
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
+  if (elInput.value.includes("0")) {
+    elInput.value = Number(elInput.value);
+  }
 });
 
 elBtn9.addEventListener("click", function () {
-    if (+elInpuit.value < 10) {
-        elInpuit.value = elInpuit.value + elBtn9.value;
-        document.getElementById('all_rounds').style.border = 'solid red' 
-      }
-      
-      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-        document.getElementById('rounds2').style.border = 'solid red'
-        document.getElementById('all_rounds').style.border = 'none'  
-    }
+  if (+elInput.value < 10) {
+    elInput.value = elInput.value + elBtn9.value;
+    document.getElementById("all_rounds").style.border = "solid red";
+  }
+
+  if (+elInput.value >= 10 && +elInput.value < 99) {
+    document.getElementById("rounds2").style.border = "solid red";
+    document.getElementById("all_rounds").style.border = "none";
+  }
+  if (elInput.value.includes("0")) {
+    elInput.value = Number(elInput.value);
+  }
 });
