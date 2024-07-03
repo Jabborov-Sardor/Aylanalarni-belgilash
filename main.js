87474514e0a621e0f5d3591d908b5b94a7aee3ed
Jabborov-Sardor <jabborovsardor0097.net@gmail.com>
2024-07-03 11:22:01 +0500

    second commit

--- main.js
+++ main.js
@@ -11,73 +11,122 @@ const elBtn8 = document.querySelector("#btn8");
 const elBtn9 = document.querySelector("#btn9");
 
 elBtn0.addEventListener("click", function () {
-//   const value = 0;
-  if (+elInpuit.value < 10) {
-    elInpuit.value = elBtn0.value;
-  }
-//   if (+elInpuit.value >= 10) {
-//     elInpuit.value = elInpuit.value + value;
-//   }
+    if (+elInpuit.value < 10) {
+        elInpuit.value = elInpuit.value + elBtn0.value;
+        document.getElementById('all_rounds').style.border = 'solid red' 
+      }
+      
+      if (+elInpuit.value > 0 && +elInpuit.value < 10 ) {
+        document.getElementById('rounds2').style.border = 'solid red'
+        document.getElementById('all_rounds').style.border = 'none'  
+    }
 });
 
 elBtn1.addEventListener("click", function () {
       if (+elInpuit.value < 10) {
         elInpuit.value = elInpuit.value + elBtn1.value;
-        document.getElementById('all_rounds').style.border = 'solid 2px red' 
+        document.getElementById('all_rounds').style.border = 'solid red' 
       }
       
       if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
-        document.getElementById('rounds2').style.border = 'solid 2px red'
+        document.getElementById('rounds2').style.border = 'solid red'
         document.getElementById('all_rounds').style.border = 'none'  
     }
     });
 
 
 elBtn2.addEventListener("click", function () {
-  const value = 2;
   if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + value;
+    elInpuit.value = elInpuit.value + elBtn2.value;
+    document.getElementById('all_rounds').style.border = 'solid red' 
   }
+  
+  if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
+    document.getElementById('rounds2').style.border = 'solid red'
+    document.getElementById('all_rounds').style.border = 'none'  
+}
 });
+
 elBtn3.addEventListener("click", function () {
-  const value = 3;
   if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + value;
+    elInpuit.value = elInpuit.value + elBtn3.value;
+    document.getElementById('all_rounds').style.border = 'solid red' 
   }
+  
+  if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
+    document.getElementById('rounds2').style.border = 'solid red'
+    document.getElementById('all_rounds').style.border = 'none'  
+}
 });
+
 elBtn4.addEventListener("click", function () {
-  const value = 4;
-  if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + value;
-  }
+    if (+elInpuit.value < 10) {
+        elInpuit.value = elInpuit.value + elBtn4.value;
+        document.getElementById('all_rounds').style.border = 'solid red' 
+      }
+      
+      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
+        document.getElementById('rounds2').style.border = 'solid red'
+        document.getElementById('all_rounds').style.border = 'none'  
+    }
 });
+
 elBtn5.addEventListener("click", function () {
-  const value = 5;
-  if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + value;
-  }
+    if (+elInpuit.value < 10) {
+        elInpuit.value = elInpuit.value + elBtn5.value;
+        document.getElementById('all_rounds').style.border = 'solid red' 
+      }
+      
+      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
+        document.getElementById('rounds2').style.border = 'solid red'
+        document.getElementById('all_rounds').style.border = 'none'  
+    }
 });
+
 elBtn6.addEventListener("click", function () {
-  const value = 6;
-  if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + value;
-  }
+    if (+elInpuit.value < 10) {
+        elInpuit.value = elInpuit.value + elBtn6.value;
+        document.getElementById('all_rounds').style.border = 'solid red' 
+      }
+      
+      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
+        document.getElementById('rounds2').style.border = 'solid red'
+        document.getElementById('all_rounds').style.border = 'none'  
+    }
 });
+
 elBtn7.addEventListener("click", function () {
-  const value = 7;
-  if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + value;
-  }
+    if (+elInpuit.value < 10) {
+        elInpuit.value = elInpuit.value + elBtn7.value;
+        document.getElementById('all_rounds').style.border = 'solid red' 
+      }
+      
+      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
+        document.getElementById('rounds2').style.border = 'solid red'
+        document.getElementById('all_rounds').style.border = 'none'  
+    }
 });
+
 elBtn8.addEventListener("click", function () {
-  const value = 8;
-  if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + value;
-  }
+    if (+elInpuit.value < 10) {
+        elInpuit.value = elInpuit.value + elBtn8.value;
+        document.getElementById('all_rounds').style.border = 'solid red' 
+      }
+      
+      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
+        document.getElementById('rounds2').style.border = 'solid red'
+        document.getElementById('all_rounds').style.border = 'none'  
+    }
 });
+
 elBtn9.addEventListener("click", function () {
-  const value = 9;
-  if (+elInpuit.value < 10) {
-    elInpuit.value = elInpuit.value + value;
-  }
+    if (+elInpuit.value < 10) {
+        elInpuit.value = elInpuit.value + elBtn9.value;
+        document.getElementById('all_rounds').style.border = 'solid red' 
+      }
+      
+      if (+elInpuit.value >= 10 && +elInpuit.value < 99 ) {
+        document.getElementById('rounds2').style.border = 'solid red'
+        document.getElementById('all_rounds').style.border = 'none'  
+    }
 });
